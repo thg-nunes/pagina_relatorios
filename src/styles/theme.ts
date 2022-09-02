c535963b6bb232a0d964460be51121f57ef50fa9
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-02 19:14:04 -0300

    style: estilos iniciais da aplicacao

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    blue: {
+      600: '#0A4B78'
+    }
+  },
+  font: {
+    sizes: {
+      1: '1rem',
+      1.5: '1.5rem',
+      2: '2rem',
+      2.5: '2.5rem',
+    },
+  },
+  spacings: {
+    5: '.5rem',
+    75: '.75rem',
+    1: '.1rem',
+    1.25: '1.25rem',
+    1.5: '1.5rem',
+    1.75: '1.75rem',
+    2: '2rem',
+  },
+}
