c8a6fe7dc9157d381da453e38032d23a717de211
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-03 12:10:59 -0300

    feat: layout ficticio de como a pagina ira ficar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { FiltersCreate } from "../client/filtersCreate"
 import { Header } from "../components/header"
+import { Report } from "../components/report"
+import * as Styled from '../styles/pages'
 
 const Home = () => {
   return (
@@ -7,6 +9,45 @@ const Home = () => {
       <Header text="Lorem" />
 
       <FiltersCreate />
+
+      <Styled.Container>
+        <Report textReport="Relatorio Estatístico - Data" printdIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+          downloadIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+        />
+        <Report textReport="Relatorio Estatístico - Data" printdIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+          downloadIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+        />
+        <Report textReport="Relatorio Estatístico - Data" printdIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+          downloadIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+        />
+        <Report textReport="Relatorio Estatístico - Data" printdIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+          downloadIcon={{
+          altImage: 'button download',
+          srcImage: '/'
+        }}
+        />
+      </Styled.Container>
     </>
   )
 }
