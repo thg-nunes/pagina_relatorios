cb1fca7649c2b39054d4ad60fde46a357238d3e9
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-03 11:34:45 -0300

    style: estilo do container de relatorios

---
+++ src/styles/pages/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  display: block;
+
+  border: 1px solid rgba(0, 0, 0, .2);
+  border-radius: 10px;
+
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, .2);
+`
