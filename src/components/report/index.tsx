b00115b4d8e68b3a8103241473b6baf7a7321a1e
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-06 20:14:25 -0300

    feat: recupera id do relatorio para fazer o download

--- src/components/report/index.tsx
+++ src/components/report/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import { useMyContextFilters } from '../../hooks/contexts/useMyContextFilters'
 import { Icon } from '../icons/print'
 import * as Styled from './styled'
 
@@ -6,12 +8,23 @@ type ReportProps = {
 }
 
 export const Report = ({ textReport }: ReportProps) => {
+  const [fileId, setFileId] = useState('')
+
+  const context = useMyContextFilters()
+  const file_id = context.state.searchByMonthOrYear
+
+  useEffect(() => {
+    if(file_id !== null) {
+      setFileId(file_id[0].id)
+    }
+  }, [])
+
   return (
     <Styled.Container>
       <span>
         <p>{textReport}</p>
         <Styled.Icons>
-          <Icon srcImage='/icons/btn-download.svg' altImage='botao para baixar relatorio' />
+          <Icon srcImage='/icons/btn-download.svg' altImage='botao para baixar relatorio' fileId={fileId} />
           <Icon srcImage='/icons/btn-print.svg' altImage='botao para tirar um print do relatorio' />
         </Styled.Icons>
       </span>
