ad7556dc82658ec4b70b3c089350b972a6b72d17
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-07 10:08:25 -0300

    feat: transforma primeira letra em uppercase

--- src/components/report/index.tsx
+++ src/components/report/index.tsx
@@ -1,28 +1,22 @@
-import { useEffect, useState } from 'react'
-import { useMyContextFilters } from '../../hooks/contexts/useMyContextFilters'
 import { Icon } from '../icons/print'
 import * as Styled from './styled'
 
 type ReportProps = {
   textReport: string
+  fileId: string
 }
 
-export const Report = ({ textReport }: ReportProps) => {
-  const [fileId, setFileId] = useState('')
+export const Report = ({ textReport, fileId }: ReportProps) => {
+  const removeingSpecialCharacters = textReport.replace(/[`_.pdf2022`]/g, ' ')
+  const replacesFirstLetterOfReportToUppercase = removeingSpecialCharacters.replace(/[`^r`]/i, 'R')
+  const replacesFirstLetterOfEstatisticaToUppercase = replacesFirstLetterOfReportToUppercase.replace('estatistica', 'Estatistica')
 
-  const context = useMyContextFilters()
-  const file_id = context.state.searchByMonthOrYear
-
-  useEffect(() => {
-    if(file_id !== null) {
-      setFileId(file_id[0].id)
-    }
-  }, [])
+  const textTreatyReport = replacesFirstLetterOfEstatisticaToUppercase
 
   return (
     <Styled.Container>
       <span>
-        <p>{textReport}</p>
+        <p>{textTreatyReport}</p>
         <Styled.Icons>
           <Icon srcImage='/icons/btn-download.svg' altImage='botao para baixar relatorio' fileId={fileId} />
           <Icon srcImage='/icons/btn-print.svg' altImage='botao para tirar um print do relatorio' />
