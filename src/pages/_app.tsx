5430b37daa2d0cf237d2164d9f732cb7f0154611
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-07 11:03:06 -0300

    feat: recupera todos os relatorios baseado no ano atual

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
 import { GlobalStyle } from '../styles/global-styles'
 import { ContextProvider } from '../contexts/contextProvider'
+import { Header } from '../components/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <ContextProvider>
+        <Header text='Lorem' />
         <Component {...pageProps} />
         <GlobalStyle />
       </ContextProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,56 @@
+import { GetStaticProps } from "next"
 import { FiltersCreate } from "../client/filtersCreate"
 import { Header } from "../components/header"
 import { Report } from "../components/report"
+import { useMyContextFilters } from "../hooks/contexts/useMyContextFilters"
+import { api } from "../services/axios"
 import * as Styled from '../styles/pages'
 
-const Home = () => {
+type AxiosResponseProps = {
+  id: string
+  file: string
+} []
+
+type Response = {
+  data: AxiosResponseProps
+  status: string
+}
+
+const Home = (response: Response) => {
+
+  const { state } = useMyContextFilters()
+
   return (
     <>
-      <Header text="Lorem" />
-
       <FiltersCreate />
 
-      <Styled.Container>
-        <Report textReport="Relatorio Estatístico - Data" />
-        <Report textReport="Relatorio Estatístico - Data" />
-        <Report textReport="Relatorio Estatístico - Data" />
-        <Report textReport="Relatorio Estatístico - Data" />
-      </Styled.Container>
+      {state.searchByMonthOrYear !== null ? (
+        <Report textReport={state.searchByMonthOrYear![0].file} fileId={state.searchByMonthOrYear[0].id} />
+      ) : (
+        <Styled.Container>
+          {response.data.map(data => (
+            <Report key={data.id} textReport={data.file} fileId={data.id} />
+          ))}
+        </Styled.Container>
+      )}
     </>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async () => {
+  const date = new Date()
+
+  const response = await api.get<Response>('/relatorios', {
+    params: {
+      year: date.getFullYear()
+    }
+  }).then(res => res.data)
+
+  return {
+    props: {
+      ...response
+    }
+  }
+}
