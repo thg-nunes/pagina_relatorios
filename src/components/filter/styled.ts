e89cdcab1a55c68e6bea6fc1ec5d6e11d6a6fa16
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-12 15:05:51 -0300

    style: adiciona position absolute

--- src/components/filter/styled.ts
+++ src/components/filter/styled.ts
@@ -34,6 +34,7 @@ type OptionsProps = {
 export const Options = styled.div<OptionsProps>`
   width: 100%;
   height: 100%;
+  position: absolute;
 
   display: ${({filterVisible}) => {
     if(filterVisible) return 'block'
