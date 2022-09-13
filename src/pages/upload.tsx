31f89672d937d9dff3064ea84625c6cc46ffa1f4
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-09-13 14:14:43 -0300

    feat: adiciona draf drop componente na pagina de upload

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -1,51 +1,45 @@
 import { useState } from "react";
-import { Filter } from "../components/filter";
-import { useMyContextFilters } from "../hooks/contexts/useMyContextFilters";
+import { DragAndDrop } from "../components/dragAndDrop";
 import { api } from "../services/axios";
 import * as Styled from '../styles/pages/upload'
 
 export default function Upload() {
-  let [file, setFile] = useState<File | string>('')
-  const context = useMyContextFilters()
-  const date = new Date()
+  const [files, setFiles] = useState<File[]>([])
 
- async function handleSendFile() {
-  const formData = new FormData()
+  async function handleSendFile() {
+    if(files.length) {
+      files.forEach(async (file) => {
+        const formData = new FormData()
+        formData.append('new_report', file)
 
-  if(file !== '') {
-    formData.append('new_report', file)
-
-    await api.post('/relatorio', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    }).then(res => console.log(res))
-  }}
+        await api.post('/relatorio', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        .then(() => {
+          alert('Arquivos enviados com sucesso!')
+          setFiles([])
+        })
+        .catch((err) => {
+          console.log(err)
+          alert('Falha ao enviar os arquivos.')
+        })
+      })
+    }
+  }
 
   return (
     <Styled.Container>
-      <Styled.YearAndMonthInput>
-        <Filter
-          labelId='year'
-          labelText='Ano'
-        />
+      <Styled.DragAndDropContainer>
+        <DragAndDrop files={files} setFiles={setFiles}/>
 
-        <Filter
-          labelId='month'
-          labelText='Mês'
-        />
-      </Styled.YearAndMonthInput>
-
-      <Styled.UploadFileInput>
-        <label htmlFor="file">Selecionar Arquivo</label>
-        <input type="file" name="selecionar arquivo para upload" id="file" onChange={(e) => {
-          setFile(e.target.files ? e.target.files[0] : '')
-        }} />
-      </Styled.UploadFileInput>
-
-      <Styled.SubmitButton>
-        <input type="submit" value="Enviar" onClick={() => handleSendFile()}/>
-      </Styled.SubmitButton>
+        <Styled.SubmitButton>
+          <input type="submit" value="Enviar" onClick={async () => {
+            await handleSendFile()
+          }}/>
+        </Styled.SubmitButton>
+      </Styled.DragAndDropContainer>
 
     </Styled.Container>
   )
