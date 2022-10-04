ac27c037976263dc3949ba007849dcb7282f7d46
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-10-04 14:46:30 -0300

    feat: implementa logica para chamar funcao de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,29 @@
-import { MyInput } from '../components/input'
+import { FormEvent, useContext, useState } from 'react'
+
+import { AuthContext } from '../contexts/authContext/authContext'
 import * as Styled from '../styles/pages/login'
 
 export default function Login() {
+
+  const { sign } = useContext(AuthContext)
+  const [ username, setUserName ] = useState<string>('')
+  const [ password, setPassword ] = useState<string>('')
+
+  async function handleSubmit(e:FormEvent) {
+    e.preventDefault()
+
+    await sign({ username, password })
+  }
+
   return (
     <Styled.Container>
-      <Styled.Form>
+      <Styled.Form onSubmit={handleSubmit}>
         <p>Acervo de Relatórios Estatísticos - JUCEMA</p>
         <Styled.InputContainer>
-          <MyInput type='text' placeholder='User' />
-          <MyInput type='password' placeholder='Senha' />
-          <button onClick={(e) => {
-            e.preventDefault()
-            // implementar a logica do login aqui
-          }}>Enviar</button>
+          <Styled.Input type='text' placeholder='User' onChange={e => setUserName(e.target.value)} />
+          <Styled.Input type='password' placeholder='Senha' onChange={e => setPassword(e.target.value)} />
         </Styled.InputContainer>
+        <button type='submit'>Enviar</button>
       </Styled.Form>
     </Styled.Container>
   )
