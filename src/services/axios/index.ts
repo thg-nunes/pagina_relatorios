125398f14140c08a018d1c30f004a1cbe9a7f5f3
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-10-07 13:47:44 -0300

    feat: config para refresh token com axios

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,7 +1,87 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from 'nookies'
+import { signOut } from "../../contexts/authContext/authContext";
+
+type FaileruRequest = {
+  onSuccess: (token: string) => void,
+  onFailure: (err: AxiosError) => void,
+}
+
+const cookies = parseCookies()
+let isRefreshing = false
+let faileruRequest: FaileruRequest[] = []
 
 const api = axios.create({
-  baseURL: `http://${process.env.NEXT_PUBLIC_API_HOST}:8001`,
+  // baseURL: `http://${process.env.NEXT_PUBLIC_API_HOST}:8001`,
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Authorization': `Bearer ${cookies['relatorio.token']}`,
+  }
+})
+
+api.interceptors.response.use((response) => {
+  return response
+}, (err: AxiosError) => {
+  if(err.response?.status === 401) {
+    const cookies = parseCookies()
+    // se tiver refreshToken nos cookies, recupero aqui
+    const { 'relatorio.refresh_token': refreshToken } = cookies
+    const requestConfig = err.config
+
+    // linhas 26 e 27: essa estrategia é utilizada para que o codigo de refreshToken seja executado somente na primeira ver em que houver um erro
+    if(!isRefreshing) {
+      isRefreshing = true
+
+      // faço uma requeset para a rota de refreshToken enviando o refreshToken
+      api.post('/relatorio/refresh_token', null, {
+        headers: {
+          'Authorization': `Bearer ${refreshToken}`
+        }
+      })
+      .then(response => {
+      // com a response de token e refreshToken atualizados, recupero-os aqui
+      const { access_token, refresh_token } = response.data
+
+        // atualizo o cookie com o novo token e refreshToken
+        setCookie(undefined, 'relatorio.token', access_token)
+        setCookie(undefined, 'relatorio.refresh_token', refresh_token)
+
+        // atualizo o header default com o novo token
+        api.defaults.headers.common['Authorization'] = `Bearer ${access_token}`
+
+        faileruRequest.forEach(request => request.onSuccess(access_token))
+        faileruRequest = []
+      })
+      .catch(err => {
+        faileruRequest.forEach(request => request.onFailure(err))
+        faileruRequest = []
+      })
+      .finally(() => {
+        isRefreshing = false
+      })
+
+      signOut()
+    }
+
+    return new Promise((resolve, reject) => {
+      faileruRequest.push({
+        // caso uma requisição falhe, vou refaze-la com o novo token
+        onSuccess: (token: string) => {
+          requestConfig.headers!['Authorization'] = `Bearer ${token}`
+
+          resolve(api(requestConfig))
+        },
+        // caso falhe com o novo token, somente rejeito
+        onFailure: (err: AxiosError) => {
+          reject(err)
+        }
+      })
+    })
+  } else {
+    signOut()
+  }
+
+  return Promise.reject(err)
 })
 
 export { api }
