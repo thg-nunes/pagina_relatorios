4b9959470e96619e2bb80ab7d2c43ce7c7079852
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-10-11 20:05:32 -0300

    feat: setando role de usuario para o localstorage

--- src/contexts/authContext/authContext.tsx
+++ src/contexts/authContext/authContext.tsx
@@ -1,7 +1,7 @@
 import Router from "next/router"
 import { stringify } from "qs"
 import { createContext, ReactNode, useEffect, useState } from "react"
-import { destroyCookie, setCookie } from 'nookies'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
 
 import { api } from "../../services/axios"
 
@@ -12,7 +12,6 @@ type AuthCredentials = {
 
 type AuthContextData = {
   sign(credentials: AuthCredentials): Promise<void>
-  role: string
 }
 
 // aqui fica a config ao sair do sistema, onde token e refresh_token sao deletados dos cookies, e o usuario é redirecionado para a pagina de login
@@ -30,7 +29,15 @@ type AuthProviderProps = {
 }
 
 export const AuthProvider = ({children}: AuthProviderProps) => {
-  const [role, setRole] = useState('user')
+
+  useEffect(() => {
+    const role = localStorage.getItem('relatorio.role')
+    const cookies = parseCookies()
+
+    if(!cookies['relatorio.token'] && role) {
+      localStorage.clear()
+    }
+  }, [])
 
   async function sign({ username, password }:AuthCredentials) {
     const dataForm = stringify({
@@ -50,6 +57,7 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
       const { access_token, refresh_token, role } = response.data
       setCookie(undefined, 'relatorio.token', access_token)
       setCookie(undefined, 'relatorio.refresh_token', refresh_token)
+      localStorage.setItem('relatorio.role', role)
 
       // o Authorization foi adicionado no headers na config criada no diretorio /services/axios/index
       api.defaults.headers['Authorization'] = `Bearer ${access_token}`
@@ -63,6 +71,6 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
 
   return (
     // aqui crio um metodo para prover a funcao de sign para usa-la onde for necessaria
-    <AuthContext.Provider value={{ sign, role }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ sign }}>{children}</AuthContext.Provider>
   )
 }
